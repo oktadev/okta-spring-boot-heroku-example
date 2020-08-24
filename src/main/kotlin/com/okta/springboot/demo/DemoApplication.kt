434772f4542ffa523c333134d43384c703302677
ruX <git@rux.vc>
2020-08-24 11:42:42 +0100

    TheController to return Hello World

--- src/main/kotlin/com/okta/springboot/demo/DemoApplication.kt
+++ src/main/kotlin/com/okta/springboot/demo/DemoApplication.kt
@@ -2,10 +2,19 @@ package com.okta.springboot.demo
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
+}
+
+@RestController
+class TheController {
+	@GetMapping("/", produces = [MediaType.TEXT_HTML_VALUE])
+	fun theAnswer(): String = "<h1>Hello, World</h1>"
 }
